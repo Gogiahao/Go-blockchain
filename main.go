be50774d5885faf2f3be23156c86ef2ad2e31283
laogahao <969097576@qq.com>
2021-08-03 16:43:04 +0800

    part2 second commit

--- main.go
+++ main.go
@@ -1,55 +1,18 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	blc "github.com/Gogiahao/Go-blockchain/blockchain"
 )
 
-type BlockChain struct {
-	Blocks []*Block
-}
-
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreatBlock(data string, PrevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), PrevHash}
-	block.DeriveHash()
-	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks) - 1]
-	newBlock := CreatBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, newBlock)
-}
-
-func Genesis() *Block {
-	return CreatBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain{
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
-	chain := InitBlockChain()
+	chain := blc.InitBlockChain()
 
 	chain.AddBlock("第一个区块")
 	chain.AddBlock("第二个区块")
 	chain.AddBlock("第三个区块")
 
-	for _, block := range chain.Blocks{
+	for _, block := range chain.Blocks {
 		fmt.Printf("PrevHash: %x\n", block.PrevHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
